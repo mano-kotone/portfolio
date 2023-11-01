194090886d77f74ad46dc7f0155b495fab0da62a
mano-kotone <147785257+mano-kotone@users.noreply.github.com>
2023-11-01 17:29:22 +0900

    topCss

--- js/function.js
+++ js/function.js
@@ -1,23 +1,60 @@
-// 36までのランダムな整数を生成
-function getRandomInt() {
+
+// message__gallery内の要素の記述
+
+const getRandomInt = () => {
     return Math.floor(Math.random() * 36) + 1;
 }
 
-// ランダムにクラス名を付与
+// ランダムなクラス名
 function assignRandomClass(element) {
     const randomClassName = `message__galleryList${getRandomInt()}`;
     element.classList.add(randomClassName);
 }
-
-// 63個のHTML要素を生成、ランダムなクラス名を付与する
+// container内53個のspan生成
 const container = document.getElementById('message__galleryContainer');
 
 for (let i = 0; i < 54; i++) {
     const element = document.createElement('span');
 
-    // クラス名を付与
+    // クラス名付与
     assignRandomClass(element);
     element.classList.add('message__galleryList');
 
     container.appendChild(element);
 }
+
+
+
+
+// 画面下部の砂嵐の生成
+const fixSunaarashi = document.getElementById('header__sunaarashi');
+console.log(fixSunaarashi )
+
+function sunaarashi() {
+    if (!!!document.createElement('canvas').getContext) {
+        return false;
+    }
+    var canvas = document.createElement('canvas'),
+        ctx = canvas.getContext('2d'),
+        x, y, c
+
+    canvas.width = 100;
+    canvas.height = 100;
+    let arry = [100, 200]
+
+    function draw() {
+        for (x = 0; x < canvas.width; x++) {
+            for (y = 0; y < canvas.height; y++) {
+                c = arry[Math.floor(Math.random() * arry.length)];
+                ctx.fillStyle = 'rgb(' + c + ',' + c + ',' + c + ')';
+                ctx.fillRect(x, y, 3, 3);
+            }
+        }
+        fixSunaarashi.style.backgroundImage = "url(" + canvas.toDataURL("image/png") + ")";
+        requestAnimationFrame(draw);
+    }
+
+    draw();
+}
+
+sunaarashi()
---
+++ js/swiper.js
@@ -0,0 +1,43 @@
+const mvSwiper = new Swiper('.mv__swiper', {
+
+    slidesPerGroup: 1,
+    loop: true,
+    effect: 'fade',
+
+
+    autoplay: {
+        delay: 10000,
+        disableOnInteraction: false,
+    },
+
+
+    pagination: {
+        el: '.mv__swiper-pagination',
+        type: 'bullets',
+        clickable: 'true'
+    },
+
+    on: {
+        transitionStart: function () {
+
+            var videos = document.querySelectorAll('video');
+
+            Array.prototype.forEach.call(videos, function (video) {
+                video.pause();
+            });
+        },
+
+        transitionEnd: function () {
+
+            var activeIndex = this.activeIndex;
+            var activeSlide = document.getElementsByClassName('mv__swiper-slide')[activeIndex];
+            var activeSlideVideo = activeSlide.getElementsByTagName('video')[0];
+            activeSlideVideo.play();
+
+        },
+
+    }
+});
+
+
+
